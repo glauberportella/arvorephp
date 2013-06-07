c59375566f060c2f321c50512a96ae246861d541
Glauber Portella <glauberportella@gmail.com>
2013-06-06 23:36:47 -0300

    Use AlloyUI

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,53 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta charset="UTF-8">
+        <title>Arvore PHP + HTML</title>
+		<link href="http://cdn.alloyui.com/2.0.0pr5/aui-css/css/bootstrap.css" rel="stylesheet">
     </head>
     <body>
-		<?php
-		// put your code here
-		?>
+		<div id="diagramContainer">
+			<div id="diagramBuilder"></div>
+		</div>
+		<script src="http://cdn.alloyui.com/2.0.0pr5/aui/aui-min.js"></script>
+		<script type="text/javascript">
+		YUI().use(
+			'aui-diagram-builder',
+			function(Y) {
+				var availableFields = [
+					{
+						iconClass: 'aui-diagram-node-task-icon',
+						label: 'Pessoa',
+						type: 'task'
+					}
+				];
+				new Y.DiagramBuilder({
+					availableFields: availableFields,
+					boundingBox: '#diagramContainer',
+					fields: [
+						{
+							name: 'João',
+							type: 'task',
+							xy: [10, 10]
+						}
+					],
+					srcNode: '#diagramBuilder'
+//					strings: {
+//						value: {
+//							addNode: 'Adicionar',
+//							cancel: 'Cancelar',
+//							close: 'Fechar',
+//							deleteConnectorsMessage: 'Tem certeza que deseja remover o(s) conectore(s) selecionado(s)?',
+//							deleteNodesMessage: 'Tem certeza que deseja remover a(s) pessoa(s) selecionada(s)?',
+//							propertyName: 'Nome da Propriedade',
+//							save: 'Salvar',
+//							settings: 'Configuração',
+//							value: 'Valor'
+//						}
+//					}
+				}).render();
+			}
+		);
+		</script>
     </body>
 </html>
